5032d18842ad1b6e59bccec117a4c8a41e61658a
william koller <wkoller25@gmail.com>
2021-10-17 20:57:37 -0300

    feat: add providers and controller in UsersModule

--- src/data/protocols/db/user/index.ts
+++ src/data/protocols/db/user/index.ts
@@ -1,1 +1,2 @@
 export * from './add-user.repository';
+export * from './load-user-by-email.repository';
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,15 @@
+import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter';
 import { UserEntity } from '@/infra/typeorm/entities/user-entity/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
+import { AddUserService } from './services/add-user/add-user.service';
+import { LoadUserByEmailService } from './services/load-user-by-email/load-user-by-email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, UsersRepository])],
+  providers: [AddUserService, LoadUserByEmailService, BcryptAdapter],
+  controllers: [UsersController],
 })
 export class UsersModule {}
